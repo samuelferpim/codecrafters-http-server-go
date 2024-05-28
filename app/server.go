400a0e76bd7bf6f99f76b971b81687ebe3930e0e
Samuel Ferreira Pimentel <s4muelpimentel@gmail.com>
2024-05-28 19:04:36 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -2,11 +2,63 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func HttpResponse(conn net.Conn, status string, body *string) {
+	mapStatus := map[string]string{
+		"200": "200 OK",
+		"404": "404 Not Found",
+	}
+
+	statusLine := "HTTP/1.1 " + mapStatus[status] + "\r\n"
+
+	if body != nil {
+		bodyLength := strconv.Itoa(len(*body))
+		headers := "Content-Type: text/plain\r\nContent-Length: " + bodyLength + "\r\n\r\n"
+		conn.Write([]byte(statusLine + headers + *body))
+	} else {
+		conn.Write([]byte(statusLine + "\r\n"))
+	}
+}
+
+func GetPathSegments(request *http.Request) []string {
+	path := request.URL.Path
+
+	segments := strings.FieldsFunc(path, func(r rune) bool {
+		return r == '/'
+	})
+
+	return segments
+}
+
+func ProcessPath(request *http.Request) ([]string, error) {
+	segments := GetPathSegments(request)
+
+	validPaths := map[string]struct{}{
+		"echo": {},
+	}
+
+	if len(segments) == 0 {
+		return []string{}, nil
+	}
+
+	if _, exists := validPaths[segments[0]]; exists {
+		if len(segments) > 1 {
+			nextSegment := segments[1]
+			return []string{segments[0], nextSegment}, nil
+		}
+		return []string{segments[0]}, nil
+	}
+
+	return []string{}, errors.New("invalid path")
+}
 
 func Handler(conn net.Conn) {
 	defer conn.Close()
@@ -16,13 +68,28 @@ func Handler(conn net.Conn) {
 		fmt.Println("Error reading request: ", err.Error())
 		return
 	}
+	fmt.Printf("Request: %v\n", request)
 
-	if request.Method == "GET" && request.URL.Path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	pathSegments, err := ProcessPath(request)
+	if err != nil {
+		HttpResponse(conn, "404", nil)
 		return
 	}
 
-	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if len(pathSegments) == 0 {
+		HttpResponse(conn, "200", nil)
+		return
+	}
+
+	if pathSegments[0] == "echo" {
+		if len(pathSegments) > 1 {
+			HttpResponse(conn, "200", &pathSegments[1])
+		} else {
+			HttpResponse(conn, "200", nil)
+		}
+	} else {
+		HttpResponse(conn, "404", nil)
+	}
 }
 
 func main() {
