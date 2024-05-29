045e3120bc69342dfa80e6a0275f55dd6ed50c96
Samuel Ferreira Pimentel <s4muelpimentel@gmail.com>
2024-05-29 20:35:08 -0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -13,36 +14,38 @@ import (
 	"strings"
 )
 
-func HttpResponse(conn net.Conn, status string, body *string, contentType string) {
-	mapStatus := map[string]string{
-		"200": "200 OK",
-		"404": "404 Not Found",
-	}
+const (
+	StatusOK                  = "200 OK"
+	StatusCreated             = "201 Created"
+	StatusNotFound            = "404 Not Found"
+	StatusInternalServerError = "500 Internal Server Error"
+	contentTypePlainText      = "text/plain"
+	contentTypeOctetStream    = "application/octet-stream"
+	bufferSize                = 4096
+)
 
-	statusLine := "HTTP/1.1 " + mapStatus[status] + "\r\n"
-
+func HttpResponse(conn net.Conn, status string, body *[]byte, contentType string) {
+	statusLine := "HTTP/1.1 " + status + "\r\n"
+	headers := "Content-Type: " + contentType + "\r\n"
 	if body != nil {
 		bodyLength := strconv.Itoa(len(*body))
-		headers := "Content-Type: " + contentType + "\r\nContent-Length: " + bodyLength + "\r\n\r\n"
-		conn.Write([]byte(statusLine + headers + *body))
+		headers += "Content-Length: " + bodyLength + "\r\n\r\n"
+		conn.Write([]byte(statusLine + headers))
+		conn.Write(*body)
 	} else {
-		conn.Write([]byte(statusLine + "\r\n"))
+		headers += "\r\n"
+		conn.Write([]byte(statusLine + headers))
 	}
 }
 
 func GetPathSegments(request *http.Request) []string {
-	path := request.URL.Path
-
-	segments := strings.FieldsFunc(path, func(r rune) bool {
+	return strings.FieldsFunc(request.URL.Path, func(r rune) bool {
 		return r == '/'
 	})
-
-	return segments
 }
 
 func ProcessPath(request *http.Request) ([]string, error) {
 	segments := GetPathSegments(request)
-
 	validPaths := map[string]struct{}{
 		"echo":       {},
 		"user-agent": {},
@@ -55,13 +58,12 @@ func ProcessPath(request *http.Request) ([]string, error) {
 
 	if _, exists := validPaths[segments[0]]; exists {
 		if len(segments) > 1 {
-			nextSegment := segments[1]
-			return []string{segments[0], nextSegment}, nil
+			return segments[:2], nil
 		}
-		return []string{segments[0]}, nil
+		return segments[:1], nil
 	}
 
-	return []string{}, errors.New("invalid path")
+	return nil, errors.New("invalid path")
 }
 
 func Handler(conn net.Conn, directory string) {
@@ -69,47 +71,104 @@ func Handler(conn net.Conn, directory string) {
 
 	request, err := http.ReadRequest(bufio.NewReader(conn))
 	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
+		fmt.Println("Error reading request:", err)
+		HttpResponse(conn, StatusInternalServerError, nil, contentTypePlainText)
 		return
 	}
-	fmt.Printf("Request: %v\n", request)
 
 	pathSegments, err := ProcessPath(request)
 	if err != nil {
-		HttpResponse(conn, "404", nil, "text/plain")
+		HttpResponse(conn, StatusNotFound, nil, contentTypePlainText)
 		return
 	}
 
 	if len(pathSegments) == 0 {
-		HttpResponse(conn, "200", nil, "text/plain")
+		HttpResponse(conn, StatusOK, nil, contentTypePlainText)
 		return
 	}
 
 	switch pathSegments[0] {
 	case "echo":
-		if len(pathSegments) > 1 {
-			HttpResponse(conn, "200", &pathSegments[1], "text/plain")
-		} else {
-			HttpResponse(conn, "200", nil, "text/plain")
-		}
+		handleEcho(conn, pathSegments)
 	case "user-agent":
-		userAgentData := request.UserAgent()
-		HttpResponse(conn, "200", &userAgentData, "text/plain")
+		handleUserAgent(conn, request)
 	case "files":
-		if len(pathSegments) > 1 {
-			file := pathSegments[1]
-			data, err := os.ReadFile(filepath.Join(directory, file))
-			if err != nil {
-				HttpResponse(conn, "404", nil, "text/plain")
+		handleFiles(conn, request, directory, pathSegments)
+	default:
+		HttpResponse(conn, StatusNotFound, nil, contentTypePlainText)
+	}
+}
+
+func handleEcho(conn net.Conn, pathSegments []string) {
+	if len(pathSegments) > 1 {
+		responseBody := []byte(pathSegments[1])
+		HttpResponse(conn, StatusOK, &responseBody, contentTypePlainText)
+	} else {
+		HttpResponse(conn, StatusOK, nil, contentTypePlainText)
+	}
+}
+
+func handleUserAgent(conn net.Conn, request *http.Request) {
+	userAgentData := request.UserAgent()
+	responseBody := []byte(userAgentData)
+	HttpResponse(conn, StatusOK, &responseBody, contentTypePlainText)
+}
+
+func handleFiles(conn net.Conn, request *http.Request, directory string, pathSegments []string) {
+	if len(pathSegments) < 2 {
+		HttpResponse(conn, StatusNotFound, nil, contentTypePlainText)
+		return
+	}
+	filePath := filepath.Join(directory, pathSegments[1])
+
+	switch request.Method {
+	case http.MethodGet:
+		file, err := os.Open(filePath)
+		if err != nil {
+			HttpResponse(conn, StatusNotFound, nil, contentTypePlainText)
+			return
+		}
+		defer file.Close()
+
+		fileInfo, err := file.Stat()
+		if err != nil {
+			HttpResponse(conn, StatusInternalServerError, nil, contentTypePlainText)
+			return
+		}
+
+		headers := "HTTP/1.1 " + StatusOK + "\r\n" +
+			"Content-Type: " + contentTypeOctetStream + "\r\n" +
+			"Content-Length: " + strconv.FormatInt(fileInfo.Size(), 10) + "\r\n\r\n"
+		conn.Write([]byte(headers))
+
+		buffer := make([]byte, bufferSize)
+		for {
+			n, err := file.Read(buffer)
+			if err != nil && err != io.EOF {
+				HttpResponse(conn, StatusInternalServerError, nil, contentTypePlainText)
 				return
 			}
-			body := string(data)
-			HttpResponse(conn, "200", &body, "application/octet-stream")
-		} else {
-			HttpResponse(conn, "404", nil, "text/plain")
+			if n == 0 {
+				break
+			}
+			conn.Write(buffer[:n])
 		}
+
+	case http.MethodPost:
+		fileContents, err := io.ReadAll(request.Body)
+		if err != nil {
+			HttpResponse(conn, StatusInternalServerError, nil, contentTypePlainText)
+			return
+		}
+		err = os.WriteFile(filePath, fileContents, 0644)
+		if err != nil {
+			HttpResponse(conn, StatusInternalServerError, nil, contentTypePlainText)
+			return
+		}
+		HttpResponse(conn, StatusCreated, nil, contentTypePlainText)
+
 	default:
-		HttpResponse(conn, "404", nil, "text/plain")
+		HttpResponse(conn, StatusNotFound, nil, contentTypePlainText)
 	}
 }
 
@@ -117,18 +176,21 @@ func main() {
 	directory := flag.String("directory", ".", "The directory to serve files from")
 	flag.Parse()
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port 4221:", err)
 		os.Exit(1)
 	}
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+	defer listener.Close()
 
+	fmt.Println("Server is listening on port 4221")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
 		go Handler(conn, *directory)
 	}
 }
